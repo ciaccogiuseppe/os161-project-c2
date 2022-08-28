31abfc763b77db6f6cc05d78434efdee021316ee
GIACOMO BRUNO s301311 <s301311@studenti.polito.it>
2022-08-28 10:41:27 +0200

    Fixed dir syscalls to pass badcall tests

--- os161/os161-base-2.0.3/kern/syscall/dir_syscalls.c
+++ os161/os161-base-2.0.3/kern/syscall/dir_syscalls.c
@@ -9,6 +9,20 @@
 #include <types.h>
 #include <copyinout.h>
 #include <kern/fcntl.h>
+#include "../arch/mips/include/vm.h"
+#include <addrspace.h>
+
+static int
+is_valid_pointer(userptr_t addr, struct addrspace *as){
+  unsigned int pointer = (unsigned int) addr;
+  if (pointer >= MIPS_KSEG0)
+    return 0;
+  if(!(((pointer >= as->as_vbase1) && (pointer < as->as_vbase1 + PAGE_SIZE*as->as_npages1))||
+  ((pointer >= as->as_vbase2) && (pointer < as->as_vbase2 + PAGE_SIZE*as->as_npages2))||
+  (pointer>=MIPS_KSEG0 - PAGE_SIZE*DUMBVM_STACKPAGES)))
+    return 0;
+  return 1;
+}
 
 //#define PATH_LEN 128
 // sys_chdir
@@ -18,6 +32,11 @@ sys_chdir(userptr_t path, int *errp){
     int err;
     struct vnode *dir;
     
+    if(path == NULL || !is_valid_pointer(path, proc_getas())){
+        *errp = EFAULT;
+        return -1;
+    }
+
     if(path != NULL && strlen((char*)path) > PATH_MAX){
         *errp = ENAMETOOLONG;
         return -1;
@@ -54,7 +73,8 @@ sys___getcwd(userptr_t buf_ptr, size_t buflen, int *errp){
     struct uio buf;
     struct iovec vec;
     if(buf_ptr == NULL){
-        *errp = EINVAL;
+        // *errp = EINVAL;
+        *errp = EFAULT;
         return -1;
     }
     uio_kinit(
